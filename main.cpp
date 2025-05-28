a7d48b9f46137b681e0d2ddd743c40b1ac6462f0
MilanSuri <milansuri19@gmail.com>
2025-05-28 11:57:00 -0400

    Changed switch case to event-driven architecture.

--- main.cpp
+++ main.cpp
@@ -1,99 +1,174 @@
 #include <iostream>
 #include "orderbook.hpp"
+#include <functional>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
 
-int main() {
-    int decision;
-    bool session = true;
-    int orderIdCounter = 1;
 
-    std::cout << "Welcome to the order book system \n";
+OrderBook orderBook;
+OrderBookBuySide buySide;
+OrderBookSellSide sellSide;
+int orderIdCounter = 1;
 
-    OrderBook orderBook;
-    OrderBookBuySide buySide;
-    OrderBookSellSide sellSide;
 
-    while (session == true){
-        std::cout << "Press [0] to leave the order book \n";
-        std::cout << "Press [1] to add a bid \n";
-        std::cout << "Press [2] to add an ask \n";
-        std::cout << "Press [3] to remove a bid \n";
-        std::cout << "Press [4] to remove an ask \n";
-        std::cout << "Press [5] to show order history \n";
-        std::cout << "Press [6] to show all orders \n";
+enum class EventType {
+    ADDBID,
+    ADDASK,
+    REMOVEBID,
+    REMOVEASK,
+    ORDERHISTORY,
+    DISPLAYORDERS,
+    UNKNOWN,
+    QUIT
+};
 
-        std::cin >> decision;
+struct Event {
+    EventType type;
+    explicit Event(EventType t) : type(t) {
 
-        switch (decision) {
-            case 0: {
-                session = false;
-                std::cout << "Thanks for using the order book system \n";
-                break;
-            } case 1: {
-                double price, quantity;
-                std::cout << "Enter the price of the bid: ";
-                std::cin >> price;
-                std::cout << "Enter the quantity: ";
-                std::cin >> quantity;
-                Order order (orderIdCounter++, price, quantity, Side::BUY);
-                orderBook.addOrder(order, sellSide, buySide);
-                break;
-            } case 2: {
-                double price, quantity;
-                std::cout << "Enter the price of the ask: ";
-                std::cin >> price;
-                std::cout << "Enter the quantity: ";
-                std::cin >> quantity;
-                Order order (orderIdCounter++, price, quantity, Side::SELL);
-                orderBook.addOrder(order, sellSide, buySide);
-                break;
-            } case 3: {
-                int orderId;
-                std::cout << "Enter the order id: ";
-                std::cin >> orderId;
-                for (auto & [price, level] : buySide.bids ) {
-                    level.removeOrder(orderId);
-                }
-                break;
-            } case 4: {
-                int orderId;
-                std::cout << "Enter the order id: ";
-                std::cin >> orderId;
-                for (auto & [price, level] : sellSide.asks) {
-                    level.removeOrder(orderId);
-                }
-                break;
-            } case 5: {
-                std::cout << "Order History:\n";
-                for (const auto& [id, order] : orderBook.orderHistory) {
-                    std::cout << "ID: " << order.getOrderId()
-                              << " | Price: " << order.getPrice()
-                              << " | Qty: " << order.getQuantity()
-                              << " | Side: " << (order.getSide() == Side::BUY ? "BUY" : "SELL") << '\n';
-                }
-                break;
-            } case 6: {
-                std::cout << "Active Buy Orders:\n";
-                for (const auto& [price, level] : buySide.bids) {
-                    std::cout << "Price: " << price << " | Total Qty: " << level.getTotalQuantity() << '\n';
-                    for (const auto& order : level.orders) {
-                        std::cout << "   ID: " << order.getOrderId() << " | Qty: " << order.getQuantity() << '\n';
-                    }
-                }
+    }
+};
 
-                std::cout << "Active Sell Orders:\n";
-                for (const auto& [price, level] : sellSide.asks) {
-                    std::cout << "Price: " << price << " | Total Qty: " << level.getTotalQuantity() << '\n';
-                    for (const auto& order : level.orders) {
-                        std::cout << "   ID: " << order.getOrderId() << " | Qty: " << order.getQuantity() << '\n';
-                    }
-                }
-                break;
-            } default: {
-                std::cout << "Invalid Choice\n";
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
+class EventDispatcher {
+    using Handler = std::function<void(const Event)>;
+    std::unordered_map<EventType, Handler> handlers;
+    public:
+
+    void registerHandler(EventType type, Handler handler) {
+        handlers[type] = handler;
+    }
+
+    void dispatch(Event event) {
+        auto it = handlers.find(event.type);
+        if (it != handlers.end()) {
+            it->second(event);
+        } else {
+            std::cout << "No handler for this event \n";
         }
     }
+};
+
+std::string toLower (std::string s) {
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
+}
+EventType parseInput(const std::string& input) {
+    std::string cmd = toLower(input);
+    if (cmd == "add bid") return EventType::ADDBID;
+    if (cmd == "add ask") return EventType::ADDASK;
+    if (cmd == "remove bid") return EventType::REMOVEBID;
+    if (cmd == "remove ask") return EventType::REMOVEASK;
+    if (cmd == "order history") return EventType::ORDERHISTORY;
+    if (cmd == "display orders") return EventType::DISPLAYORDERS;
+    if (cmd == "quit") return EventType::QUIT;
+    return EventType::UNKNOWN;
+}
+
+int main() {
+
+
+    EventDispatcher dispatcher;
+
+    // Register event handlers
+    dispatcher.registerHandler(EventType::ADDBID, [](const Event&) {
+        double price, quantity;
+        std::cout << "Enter the price of the bid: ";
+        std::cin >> price;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Enter the quantity: ";
+        std::cin >> quantity;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        Order order (orderIdCounter++, price, quantity, Side::BUY);
+        orderBook.addOrder(order, sellSide, buySide);
+        std::cout << "Adding bid\n";
+    });
+
+    dispatcher.registerHandler(EventType::ADDASK, [](const Event&) {
+        std::cout << "Adding ask\n";
+        double price, quantity;
+        std::cout << "Enter the price of the ask: ";
+        std::cin >> price;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Enter the quantity: ";
+        std::cin >> quantity;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        Order order (orderIdCounter++, price, quantity, Side::SELL);
+        orderBook.addOrder(order, sellSide, buySide);
+    });
+
+    dispatcher.registerHandler(EventType::REMOVEBID, [](const Event&) {
+        std::cout << "Removing bid\n";
+        int orderId;
+        std::cout << "Enter the order id: ";
+        std::cin >> orderId;
+        for (auto & [price, level] : buySide.bids ) {
+            level.removeOrder(orderId);
+        }
+    });
+
+    dispatcher.registerHandler(EventType::REMOVEASK, [](const Event&) {
+      std::cout << "Removing ask\n";
+        int orderId;
+              std::cout << "Enter the order id: ";
+              std::cin >> orderId;
+              for (auto & [price, level] : sellSide.asks) {
+                  level.removeOrder(orderId);
+              }
+  });
+
+    dispatcher.registerHandler(EventType::ORDERHISTORY, [](const Event&) {
+        std::cout << "Showing order history\n";
+        std::cout << "Order History:\n";
+        for (const auto& [id, order] : orderBook.orderHistory) {
+            std::cout << "ID: " << order.getOrderId()
+            << " | Price: " << order.getPrice()
+            << " | Qty: " << order.getQuantity()
+            << " | Side: " << (order.getSide() == Side::BUY ? "BUY" : "SELL") << '\n';
+              }
+  });
+
+    dispatcher.registerHandler(EventType::DISPLAYORDERS, [](const Event&) {
+        std::cout << "Displaying orders\n";
+        std::cout << "Active Buy Orders:\n";
+               for (const auto& [price, level] : buySide.bids) {
+                   std::cout << "Price: " << price << " | Total Qty: " << level.getTotalQuantity() << '\n';
+                   for (const auto& order : level.orders) {
+                       std::cout << "   ID: " << order.getOrderId() << " | Qty: " << order.getQuantity() << '\n';
+                   }
+               }
+
+               std::cout << "Active Sell Orders:\n";
+               for (const auto& [price, level] : sellSide.asks) {
+                   std::cout << "Price: " << price << " | Total Qty: " << level.getTotalQuantity() << '\n';
+                   for (const auto& order : level.orders) {
+                       std::cout << "   ID: " << order.getOrderId() << " | Qty: " << order.getQuantity() << '\n';
+                   }
+               }
+  });
+
+    dispatcher.registerHandler(EventType::UNKNOWN, [](const Event&) {
+    std::cout << "Unknown command. Please try again.\n";
+});
+
+    dispatcher.registerHandler(EventType::QUIT, [](const Event&) {
+       std::cout << "Quitting\n";
+    });
+
+    std::string input;
+    bool running = true;
+
+    while (running) {
+        std::cout << "Enter command (add bid, add ask, remove bid, remove ask, order history, display orders, quit): ";
+        std::getline(std::cin, input);
+        EventType type = parseInput(input);
+        Event event(type);
+        dispatcher.dispatch(event);
+        if (type == EventType::QUIT) {
+            running = false;
+        }
+    }
+
     return 0;
 }
+
